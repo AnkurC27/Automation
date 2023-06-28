2b8511caf6ac8bc4fa8ba6f6191d92da63992af2
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-06-28 15:05:45 -0700

    updating chat gpt code

--- Resources/chatgptcode.py
+++ Resources/chatgptcode.py
@@ -1,25 +1,19 @@
-#sample python code for automation
-
-
-
-
-
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
 import datetime
 import pandas as pd
+from selenium import webdriver
 
-# Set the path to the ChromeDriver executable
-chromedriver_path = '/path/to/chromedriver'
+# Set the path to the EdgeDriver executable
+edgedriver_path = '/path/to/msedgedriver'  # Replace with the actual path to msedgedriver
 
-# Set up the Chrome options
-chrome_options = webdriver.ChromeOptions()
+# Set up the Edge options
+edge_options = webdriver.EdgeOptions()
 # Add any desired options, e.g., to run headless:
-# chrome_options.add_argument('--headless')
+# edge_options.add_argument('--headless')
 
-# Initialize the Chrome driver
-driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+# Initialize the Edge driver
+driver = webdriver.Edge(executable_path=edgedriver_path, options=edge_options)
 
 # List of target websites
 websites = [
@@ -35,26 +29,44 @@ manufacturer_codes_df = pd.read_excel(excel_file_path)
 # Get the current date and time
 current_datetime = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-# Loop through the manufacturer codes
+# Iterate over the manufacturer codes
 for index, row in manufacturer_codes_df.iterrows():
-    manufacturer_code = row['Manufacturer Code']
+    manufacturer_code_1 = row['Manufacturer Code 1']
+    manufacturer_code_2 = row['Manufacturer Code 2']
 
     # Loop through the websites
     for i, website in enumerate(websites):
-        # Construct the URL
-        url = website + manufacturer_code
+        # Construct the URL using manufacturer code 1
+        url_1 = website + manufacturer_code_1
 
         # Fetch the web page
-        response = requests.get(url)
+        response_1 = requests.get(url_1)
 
         # Create a BeautifulSoup object
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup_1 = BeautifulSoup(response_1.text, 'html.parser')
 
-        # Capture the screenshot
-        screenshot_filename = f'screenshot_{manufacturer_code}_{i}_{current_datetime}.png'
-        driver.get("data:text/html;charset=utf-8," + str(soup))
-        driver.save_screenshot(screenshot_filename)
+        # Check if the web page contains the desired content
+        if 'No results found' not in response_1.text:
+            # Capture the screenshot using manufacturer code 1
+            screenshot_filename_1 = f'screenshot_{manufacturer_code_1}_{i}_{current_datetime}.png'
+            driver.get("data:text/html;charset=utf-8," + str(soup_1))
+            driver.save_screenshot(screenshot_filename_1)
+        else:
+            # Construct the URL using manufacturer code 2
+            url_2 = website + manufacturer_code_2
+
+            # Fetch the web page
+            response_2 = requests.get(url_2)
+
+            # Create a BeautifulSoup object
+            soup_2 = BeautifulSoup(response_2.text, 'html.parser')
+
+            # Capture the screenshot using manufacturer code 2
+            screenshot_filename_2 = f'screenshot_{manufacturer_code_2}_{i}_{current_datetime}.png'
+            driver.get("data:text/html;charset=utf-8," + str(soup_2))
+            driver.save_screenshot(screenshot_filename_2)
 
 # Close the web driver
 driver.quit()
+
 
