42f759d3ba51d88015559c45e152c42fe82dab54
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-06-29 15:54:30 -0700

    updates

--- Resources/chatgptcode.py
+++ Resources/chatgptcode.py
@@ -3,47 +3,48 @@ import requests
 import datetime
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 
 # Set the path to the EdgeDriver executable
-edgedriver_path = 'C:\Users\ankur.chadha\desktop\msedgedriver'  # Replace with the actual path to msedgedriver
+edgedriver_path = r'C:\Users\ankur.chadha\desktop\msedgedriver'  # Replace with the actual path to msedgedriver
 
 # Set up the Edge options
 edge_options = webdriver.EdgeOptions()
 # Add any desired options, e.g., to run headless:
 # edge_options.add_argument('--headless')
 
-# Initialize the Edge driver
-driver = webdriver.Edge(executable_path=edgedriver_path, options=edge_options)
+# Initialize the Edge driver with the EdgeOptions object
+driver = webdriver.Edge(service=Service(edgedriver_path), options=edge_options)
 
 # List of target websites
-websites = [https://www.bing.com/shop?FORM=SHOPTB]
+websites = ['https://www.bing.com/shop?FORM=SHOPTB']
 
 # Read manufacturer codes from Excel file
-excel_file_path = 'C:\Users\ankur.chadha\Desktop\Automation\sku_test.xlsx'  # Update with your Excel file path
+excel_file_path = r'C:\Users\ankur.chadha\Desktop\Automation\SKU test.xlsx'  # Update with your Excel file path
 sku_test_df = pd.read_excel(excel_file_path)
 
 # Get the current date and time
 current_datetime = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
+ # Construct the screenshot filenames based on item description
+    screenshot_filename_1 = f'screenshot_{item_description}_1_{current_datetime}.png'
+    screenshot_filename_2 = f'screenshot_{item_description}_2_{current_datetime}.png'
+    
+
 # Iterate over the manufacturer codes
 for index, row in sku_test_df.iterrows():
-    manufacturer_code_1 = row['Manufacturer Code 1']
-    manufacturer_code_2 = row['Manufacturer Code 2']
+    model_number_1 = row['Manufacturer Code 1']
+    model_number_2 = row['Manufacturer Code 2']
     item_description = row['item description']
 
-#skip iteration if manufacturer codes are missing
-if pd.isnull(manufacturer_code_1) or pd.isnull(manufacturer_code_2):
-continue
-
-
- # Construct the screenshot filename based on item description
-screenshot_filename_1 = f'screenshot_{item_description}_{i}_{current_datetime}.png'
-driver.save_screenshot(screenshot_filename_1)
+    # Skip iteration if manufacturer codes are missing
+    if pd.isnull(model_number_1) or pd.isnull(model_number_2):
+        continue
 
     # Loop through the websites
     for i, website in enumerate(websites):
         # Construct the URL using manufacturer code 1
-        url_1 = website + manufacturer_code_1
+        url_1 = website + model_number_1
 
         # Fetch the web page
         response_1 = requests.get(url_1)
@@ -54,12 +55,12 @@ driver.save_screenshot(screenshot_filename_1)
         # Check if the web page contains the desired content
         if 'No results found' not in response_1.text:
             # Capture the screenshot using manufacturer code 1
-            screenshot_filename_1 = f'screenshot_{manufacturer_code_1}_{i}_{current_datetime}.png'
+            screenshot_filename_1 = f'screenshot_{model_number_1}_{i}_{current_datetime}.png'
             driver.get("data:text/html;charset=utf-8," + str(soup_1))
             driver.save_screenshot(screenshot_filename_1)
         else:
             # Construct the URL using manufacturer code 2
-            url_2 = website + manufacturer_code_2
+            url_2 = website + model_number_2
 
             # Fetch the web page
             response_2 = requests.get(url_2)
@@ -68,11 +69,10 @@ driver.save_screenshot(screenshot_filename_1)
             soup_2 = BeautifulSoup(response_2.text, 'html.parser')
 
             # Capture the screenshot using manufacturer code 2
-            screenshot_filename_2 = f'screenshot_{manufacturer_code_2}_{i}_{current_datetime}.png'
+            screenshot_filename_2 = f'screenshot_{model_number_2}_{i}_{current_datetime}.png'
             driver.get("data:text/html;charset=utf-8," + str(soup_2))
             driver.save_screenshot(screenshot_filename_2)
 
 # Close the web driver
 driver.quit()
-
 
