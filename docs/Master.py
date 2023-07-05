71b36559b7b2f50280d2020f756db0c1a3ba5c2f
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-07-05 09:08:02 -0700

    adjustments

--- docs/Master.py
+++ docs/Master.py
@@ -47,29 +47,31 @@ for index, row in sku_test_df.iterrows():
     if pd.isnull(model_number_1) and pd.isnull(model_number_2):
         continue
     
-    # Generate the website URLs by replacing the placeholders with the model numbers
-    website_url_1 = website_url_template.replace('{model_number}', str(model_number_1))
+    # Check if model number 1 is not null before taking screenshot
+    if pd.notnull(model_number_1):
+        # Generate the website URLs by replacing the placeholders with the model numbers
+        website_url_1 = website_url_template.replace('{model_number}', str(model_number_1))
 
-    # Capture the screenshot using manufacturer code 1
-    screenshot_filename_1 = f'screenshot_{item_number_col_index}_{item_description_col_index}.png'
-    driver.get(website_url_1)
-    driver.save_screenshot(screenshot_filename_1)
+        # Capture the screenshot using manufacturer code 1
+        driver.get(website_url_1)
+        screenshot_filename_1 = f'screenshot_{row[item_number_col_index]}_{row[item_description_col_index]}.png'
+        driver.save_screenshot(screenshot_filename_1)
 
-else: 
+    # Check if model number 2 is not null before taking screenshot
+    if pd.notnull(model_number_2):
+        # Construct the URL using manufacturer code 2
+        website_url_2 = website_url_template.replace('{model_number}', str(model_number_2))
+        
+        # Fetch the web page
+        response_2 = requests.get(website_url_2)
 
-    # Construct the URL using manufacturer code 2
-    website_url_2 = website_url_template + str(model_number_2)
-    website_url_2 = website_url_template.replace('{model_number}', str(model_number_2))
-    
-    # Fetch the web page
-    response_2 = requests.get(website_url_2)
+        # Create a BeautifulSoup object
+        soup_2 = BeautifulSoup(response_2.text, 'html.parser')
 
-    # Create a BeautifulSoup object
-    soup_2 = BeautifulSoup(response_2.text, 'html.parser')
-
-    # Capture the screenshot using manufacturer code 2
-    screenshot_filename_2 = f'screenshot_{model_number_1}.png'
-    driver.get(website_url_2)
-    driver.save_screenshot(screenshot_filename_2)
+        # Capture the screenshot using manufacturer code 2
+        driver.get(website_url_2)
+        screenshot_filename_2 = f'screenshot_{model_number_2}.png'
+        driver.save_screenshot(screenshot_filename_2)
 
 driver.quit()
+
