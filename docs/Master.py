8a481ee79ced916035cd897a5826b7a726d0a9ed
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-07-10 14:29:26 -0700

    update

--- docs/Master.py
+++ docs/Master.py
@@ -40,63 +40,57 @@ model_number_col_index_2 = sku_test_df.columns.get_loc('Model Number 2')
 # Get the column index of the 'Item Number' header
 item_number_col_index = sku_test_df.columns.get_loc('Item Number')
 
-# Get the column index of the 'Model Number' header
-item_description_col_index = sku_test_df.columns.get_loc('Item Description')
+
+def add_watermark(screenshot_filename):
+    # Add watermark to screenshot
+    img = Image.open(screenshot_filename)
+    draw = ImageDraw.Draw(img)
+    text = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    position = (10, 10)
+    font = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", 30)
+    color = "black" 
+    
+    # Get image and text dimensions
+    img_width, img_height = img.size
+    text_width, text_height = draw.textsize(text, font=font)
+
+    position = ((img_width - text_width) // 2, 10)
+
+    draw.text(position, text, font=font, fill=color)
+    img.save(screenshot_filename)
 
 # Iterate over the rows in the Excel file
 for index, row in sku_test_df.iterrows():
     model_number_1 = row[model_number_col_index]
     model_number_2 = row[model_number_col_index_2]
+    item_number = row[item_number_col_index]
+    folder_name = str(int(item_number/100)*100)
 
-    print(f'Processing row{index}: model_number_1={model_number_1}, model_number_2={model_number_2}')
-    
-    # Skip iteration if both model numbers are missing
-    if pd.isnull(model_number_1) and pd.isnull(model_number_2):
-        continue
-    
+    if not os.path.exists(folder_name):
+        try:
+            os.mkdir(folder_name)
+            print(f"Directory '{folder_name}' created.")
+        except Exception as e:
+            print(f"Could not create the directory. Error{str(e)}")
+
+    # Generate the website URLs by replacing the placeholders with the model numbers
+    website_url_1 = website_url_template.replace('{model_number}', str(model_number_1))
+    website_url_2 = website_url_template.replace('{model_number}', str(model_number_2))
+
     # Check if model number 1 is not null before taking screenshot
     if pd.notnull(model_number_1):
-        # Generate the website URLs by replacing the placeholders with the model numbers
-        website_url_1 = website_url_template.replace('{model_number}', str(model_number_1))
-
         # Capture the screenshot using manufacturer code 1
         driver.get(website_url_1)
-        screenshot_filename_1 = f'screenshot_{row[item_number_col_index]}_{row[item_description_col_index]}.png'
-        driver.save_screenshot(screenshot_filename_1)
-
-        # Add Watermark to screenshot
-        img = Image.open(screenshot_filename_1)
-        draw = ImageDraw.Draw(img)
-        text = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        position = (10, 10)
-        font = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", 15)
-        draw.text(position, text, font=font)
-        img.save(screenshot_filename_1)
-
+        screenshot_filename = f'{folder_name}/screenshot_{row["Item Number"]}_{row["Item Description"]}.png'
+        if driver.save_screenshot(screenshot_filename):
+            add_watermark(screenshot_filename)
     # Check if model number 2 is not null before taking screenshot
-    if pd.notnull(model_number_2):
-        # Construct the URL using manufacturer code 2
-        website_url_2 = website_url_template.replace('{model_number}', str(model_number_2))
-        
-        # Fetch the web page
-        response_2 = requests.get(website_url_2)
-
-        # Create a BeautifulSoup object
-        soup_2 = BeautifulSoup(response_2.text, 'html.parser')
-
+    elif pd.notnull(model_number_2):
         # Capture the screenshot using manufacturer code 2
         driver.get(website_url_2)
-        screenshot_filename_2 = f'screenshot_{model_number_2}.png'
-        driver.save_screenshot(screenshot_filename_2)
-
-         # Add Watermark to screenshot
-        img = Image.open(screenshot_filename_2)
-        draw = ImageDraw.Draw(img)
-        text = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        position = (10, 10)
-        font = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", 15)
-        draw.text(position, text, font=font)
-        img.save(screenshot_filename_2)
+        screenshot_filename = f'{folder_name}/screenshot_{row["Item Number"]}_{row["Item Description"]}.png'
+        if driver.save_screenshot(screenshot_filename):
+            add_watermark(screenshot_filename)
 
 driver.quit()
 
