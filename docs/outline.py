b4726041d8908112cd7ea404ad290c1fc7ac5f3a
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-07-19 13:54:28 -0700

    border 2

--- docs/outline.py
+++ docs/outline.py
@@ -67,7 +67,17 @@ website_columns = {
 # also adds a black border around the image
 def add_watermark(screenshot_filename, item_number, item_desc):
     img = Image.open(screenshot_filename)
-    draw = ImageDraw.Draw(img)
+
+    #width of border in pixels
+    border_width = 10
+
+    # create a new image with size increased by twice the border width
+    new_img = Image.new("RGB", (img.width +2 * border_width, img.height +2 * border_width), "black")
+
+    # paste the original image at an offset of the border within
+    new_img.paste(img, (border_width, border_width))
+
+    draw = ImageDraw.Draw(new_img)
     date_time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     watermark_text = f"{item_number}, {item_desc}, {date_time}"
     position = (10, 10)
@@ -80,10 +90,10 @@ def add_watermark(screenshot_filename, item_number, item_desc):
     img_width, img_height = img.size
     text_width, text_height = draw.textsize(watermark_text, font=font)
 
-    position = (img_width - text_width - 10, img_height - text_height -10)
+    position = (new_img.width - text_width - 10, new_img.height - text_height -10)
 
     draw.text(position, watermark_text, font=font, fill=color, stroke_width=stroke_width, stroke_fill=stroke_color)
-    img.save(screenshot_filename)
+    new_img.save(screenshot_filename)
 
 
 # Iterate over the rows in the Excel file
