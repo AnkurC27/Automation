cc0718f637671f9f6dd04efb9dc094c8e65fef20
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-07-24 12:47:31 -0700

    colorado lumber

--- docs/Excel reader.py
+++
@@ -1,8 +0,0 @@
-
-
-# Read manufacturer codes from Excel file
-excel_file_path = r'C:\Users\ankur.chadha\Desktop\Automation\skutest.xlsx'  # Update with your Excel file path
-sku_test_df = pd.read_excel(excel_file_path)
-
-
-    # Rest of the code to perform scraping or other operations with the manufacturer codes
